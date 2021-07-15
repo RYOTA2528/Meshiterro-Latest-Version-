fea972a816968192b5bf83794ea1cc25211fa651
RYOTA2528 <1216022@gmail.com>
2021-07-15 17:53:52 +0000

    [Add]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '73492cc28d5c0464b474a4354a9fe504a83f45e82a2b940df9f9d99c16e78025587920487eb35f80c1451234a887976df609498fb1beadd54e42fb6c5847d43b'
+
